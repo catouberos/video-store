170f18504271efb80d16026e22926a1041a46033
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-04-07 00:58:31 +0700

    feat: implement `Item` class

---
+++ src/main/java/pls_no_shinobu/videostore/model/Item.java
@@ -0,0 +1,167 @@
+/*
+  RMIT University Vietnam
+  Course: INTE2512 Object-Oriented Programming
+  Semester: 2023A
+  Assessment: Final Project
+  Author: pls_no_shinobu
+*/
+package pls_no_shinobu.videostore.model;
+
+import java.util.HashSet;
+
+public class Item extends Entity {
+    public enum RentalType {
+        RECORD,
+        DVD,
+        GAME
+    }
+
+    public enum LoanType {
+        TWO_DAY,
+        ONE_WEEK
+    }
+
+    public enum RentalStatus {
+        BORROWED,
+        AVAILABLE
+    }
+
+    private String title;
+    private HashSet<String> genres;
+    private RentalType rentalType;
+    private LoanType loanType;
+    private int stock;
+    private float rentalFee;
+    private RentalStatus rentalStatus;
+
+    public Item() {
+        super();
+        this.title = "";
+        this.genres = new HashSet<>();
+        this.rentalType = RentalType.RECORD;
+        this.loanType = LoanType.ONE_WEEK;
+        this.stock = 0;
+        this.rentalFee = 0;
+        this.rentalStatus = RentalStatus.AVAILABLE;
+    }
+
+    public Item(
+            String id,
+            String title,
+            HashSet<String> genres,
+            RentalType rentalType,
+            LoanType loanType,
+            int stock,
+            int rentalFee,
+            RentalStatus rentalStatus)
+            throws IllegalArgumentException {
+        setId(id);
+        this.title = title;
+        this.genres.addAll(genres);
+        this.rentalType = rentalType;
+        this.loanType = loanType;
+        this.stock = stock;
+        this.rentalFee = rentalFee;
+        this.rentalStatus = rentalStatus;
+    }
+
+    public void setId(String id) throws IllegalArgumentException {
+        if (!id.matches("^I\\d{3}-\\d{1,4}$")) {
+            throw new IllegalArgumentException();
+        }
+
+        super.setId(id);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean setTitle(String title) {
+        this.title = title;
+
+        return true;
+    }
+
+    public RentalType getRentalType() {
+        return rentalType;
+    }
+
+    public boolean setRentalType(RentalType rentalType) {
+        this.rentalType = rentalType;
+
+        return true;
+    }
+
+    public LoanType getLoanType() {
+        return loanType;
+    }
+
+    public boolean setLoanType(LoanType loanType) {
+        this.loanType = loanType;
+
+        return true;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public boolean setStock(int stock) {
+        // Stock must be larger than 0
+        if (stock < 0) {
+            return false;
+        }
+
+        this.stock = stock;
+
+        return true;
+    }
+
+    public float getRentalFee() {
+        return rentalFee;
+    }
+
+    public boolean setRentalFee(float rentalFee) {
+        if (rentalFee < 0) {
+            return false;
+        }
+
+        this.rentalFee = rentalFee;
+
+        return true;
+    }
+
+    public RentalStatus getRentalStatus() {
+        return rentalStatus;
+    }
+
+    public boolean setRentalStatus(RentalStatus rentalStatus) {
+        this.rentalStatus = rentalStatus;
+
+        return true;
+    }
+
+    public HashSet<String> getGenres() {
+        return genres;
+    }
+
+    public boolean addGenre(String genre) {
+        if (genres.contains(genre)) {
+            return false;
+        }
+
+        genres.add(genre);
+
+        return true;
+    }
+
+    public boolean removeGenre(String genre) {
+        if (!genres.contains(genre)) {
+            return false;
+        }
+
+        genres.remove(genre);
+        return true;
+    }
+}
