b46b88bfaaa5375ec312849c322853cdd91e560c
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-04-06 17:00:28 +0700

    refactor: code reformatted with `spotless`

--- src/main/java/pls_no_shinobu/videostore/HelloController.java
+++ src/main/java/pls_no_shinobu/videostore/HelloController.java
@@ -12,8 +12,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
