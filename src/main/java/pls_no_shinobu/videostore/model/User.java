dbb312b5ff23f7afe3dbad9051877e1c98371384
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-04-08 18:52:03 +0700

    feat: refactor variables and implement checks

--- src/main/java/pls_no_shinobu/videostore/model/User.java
+++ src/main/java/pls_no_shinobu/videostore/model/User.java
@@ -10,91 +10,173 @@ package pls_no_shinobu.videostore.model;
 import java.util.ArrayList;
 
 public class User extends Entity {
-    public enum AccountType {
+    public enum UserType {
         GUEST,
         REGULAR,
-        VIP
+        VIP,
+        ADMIN
     }
 
     private String name;
-    private String userAddress;
-    private String userPhone;
-    private String userName;
-    private String userPassword;
-    private AccountType role;
-    private int rolePoint = 0;
-    private ArrayList<Item> rentedItems = new ArrayList<Item>();
+    private String address;
+    private String phone;
+    private int rentalCount;
+    private UserType role;
+    private String username;
+    private String password;
+    private ArrayList<String> rentals;
 
-    public User(String id, String userName, String userPassword) {
-        super(id);
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.role = AccountType.GUEST;
+    public User() {
+        super();
+        this.name = "";
+        this.address = "";
+        this.phone = "";
+        this.rentalCount = 0;
+        this.role = UserType.GUEST;
+        this.username = "";
+        this.password = "";
+        this.rentals = new ArrayList<>();
+    }
+
+    public User(String id, String username, String password) {
+        setId(id);
+        setUsername(username);
+        setPassword(password);
     }
 
     public User(
             String id,
-            String userName,
-            String userPassword,
-            String userAddress,
-            String userPhone,
-            String name) {
-        this(id, userName, userPassword);
-        this.userAddress = userAddress;
-        this.userPhone = userPhone;
+            String name,
+            String address,
+            String phone,
+            int rentalCount,
+            UserType userType,
+            String username,
+            String password,
+            ArrayList<String> rentals)
+            throws IllegalArgumentException {
+        setId(id);
         this.name = name;
-        this.role = AccountType.GUEST;
+        this.address = address;
+        this.phone = phone;
+        this.rentalCount = rentalCount;
+        this.role = userType;
+        setUsername(username);
+        setPassword(password);
+        this.rentals = rentals;
     }
 
-    public String getUserName() {
-        return userName;
-    }
+    @Override
+    public void setId(String id) throws IllegalArgumentException {
+        if (!id.matches("^C\\d{3}$")) {
+            throw new IllegalArgumentException("Invalid user ID format");
+        }
 
-    public void setUserName(String username) {
-        this.userName = username;
-    }
-
-    public String getPassword() {
-        return userPassword;
-    }
-
-    public void setPassword(String password) {
-        this.userPassword = password;
-    }
-
-    public String getAddress() {
-        return userAddress;
-    }
-
-    public void setAddress(String address) {
-        this.userAddress = address;
-    }
-
-    public String getPhone() {
-        return userPhone;
-    }
-
-    public void setPhone(String phone) {
-        this.userPhone = phone;
+        super.setId(id);
     }
 
     public String getName() {
-        return userName;
+        return name;
     }
 
-    public void setName(String name) {
-        this.userName = name;
+    public boolean setName(String name) {
+        this.name = name;
+
+        return true;
     }
 
-    public AccountType getRole() {
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean setAddress(String address) {
+        this.address = address;
+
+        return true;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public boolean setPhone(String phone) {
+        this.phone = phone;
+
+        return true;
+    }
+
+    public int getRentalCount() {
+        return rentalCount;
+    }
+
+    public boolean setRentalCount(int rentalCount) {
+        this.rentalCount = rentalCount;
+
+        return true;
+    }
+
+    public int increateRentalCount() {
+        this.rentalCount++;
+
+        return rentalCount;
+    }
+
+    public UserType getRole() {
         return role;
     }
 
-    public int getPoints() {
-        return rolePoint;
+    public boolean setRole(UserType role) {
+        this.role = role;
+
+        return true;
     }
 
-    public ArrayList<Item> getRentedItems() {
-        return rentedItems;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) throws IllegalArgumentException {
+        if (username.isBlank()) throw new IllegalArgumentException("Username cannot be empty");
+
+        if (username.contains("\\s"))
+            throw new IllegalArgumentException("Username cannot contain spaces");
+
+        this.username = username;
+    }
+
+    protected String getPassword() {
+        return password;
+    }
+
+    protected void setPassword(String password) throws IllegalArgumentException {
+        if (username.isEmpty()) throw new IllegalArgumentException("Password cannot be empty");
+
+        this.password = password;
+    }
+
+    public ArrayList<String> getRentals() {
+        return rentals;
+    }
+
+    public boolean setRentals(ArrayList<String> rentals) {
+        this.rentals = rentals;
+
+        return true;
+    }
+
+    public boolean addRentals(String rId) {
+        // TODO: add stock check
+
+        this.rentals.add(rId);
+
+        return true;
+    }
+
+    public boolean removeRentals(String rId) {
+        if (!this.rentals.contains(rId)) return false;
+
+        this.rentals.remove(rId);
+
+        return true;
     }
 }
