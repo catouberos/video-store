9b37148b726bd635d71c3dbb4d570a914f7370e7
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-04-18 12:46:01 +0700

    docs: add docs for `Entity`

--- src/main/java/pls_no_shinobu/videostore/model/Entity.java
+++ src/main/java/pls_no_shinobu/videostore/model/Entity.java
@@ -7,21 +7,46 @@
 */
 package pls_no_shinobu.videostore.model;
 
+/**
+ * Abstract classs for building an entity, which requires an ID that can be extended
+ *
+ * @author Do Khoa Nguyen
+ */
 public abstract class Entity {
     private String id;
 
+    /**
+     * Constructor for setting the default value of {@link Entity}
+     *
+     * @author Do Khoa Nguyen
+     */
     public Entity() {
         this.id = "";
     }
 
+    /**
+     * Constructor for an {@link Entity} with ID
+     *
+     * @author Do Khoa Nguyen
+     */
     public Entity(String id) {
         setId(id);
     }
 
+    /**
+     * Method for setting the {@link Entity}'s ID
+     *
+     * @author Do Khoa Nguyen
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Method to get the {@link Entity} ID
+     *
+     * @author Do Khoa Nguyen
+     */
     public String getId() {
         return id;
     }
