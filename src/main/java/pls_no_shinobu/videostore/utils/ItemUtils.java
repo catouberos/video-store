7baefc33d84b93680a130393a3434ce5b96ababd
Nguyen Do <khoanguyen.do@outlook.com>
2023-04-20 23:01:20 +0700

    feat: implement serialize and add docs for `ItemUtils` (#42)
    * feat: implement serialize and add docs
    
    * test: add serialize test for `ItemUtils`
    
    * feat: implement `LoanType` `toString()`

--- src/main/java/pls_no_shinobu/videostore/utils/ItemUtils.java
+++ src/main/java/pls_no_shinobu/videostore/utils/ItemUtils.java
@@ -12,11 +12,15 @@ import pls_no_shinobu.videostore.model.Item;
 /**
  * Utilities for parsing, and serialize Item object into CSV-compatible file
  *
- * @see CSVUtils
  * @author Do Khoa Nguyen
  */
-public class ItemUtils implements CSVUtils<Item> {
-    @Override
+public class ItemUtils {
+    /**
+     * Method to parse an {@link Item} from CSV-like file. <br>
+     * File format: {@code id,title,rentType,loanType,stock,rentalFee[,genre]}
+     *
+     * @author Do Khoa Nguyen
+     */
     public Item parse(String str) {
         try {
             String[] tokens = str.split("\\s*,\\s*");
@@ -26,7 +30,6 @@ public class ItemUtils implements CSVUtils<Item> {
             Item.RentalType rentalType = parseRentalType(tokens[2]);
             Item.LoanType loanType = parseLoanType(tokens[3]);
             int stock = Integer.parseInt(tokens[4]);
-            System.out.print(tokens[5] + Float.parseFloat(tokens[5]));
             float rentalFee = Float.parseFloat(tokens[5]);
             String genre = tokens.length == 7 ? tokens[6] : null;
 
@@ -37,10 +40,25 @@ public class ItemUtils implements CSVUtils<Item> {
         }
     }
 
-    @Override
+    /**
+     * Method to serialize an {@link Item} to CSV-compatible file. <br>
+     * File format: {@code id,title,rentType,loanType,stock,rentalFee[,genre]}
+     *
+     * @author Do Khoa Nguyen
+     */
     public String serialize(Item item) {
+        if (item.getGenre() == null)
+            return String.format(
+                    "%s,%s,%s,%s,%d,%.2f",
+                    item.getId(),
+                    item.getTitle(),
+                    item.getRentalType().toString(),
+                    item.getLoanType().toString(),
+                    item.getStock(),
+                    item.getRentalFee());
+
         return String.format(
-                "%s,%s,%s,%s,%d,%f,%s",
+                "%s,%s,%s,%s,%d,%.2f,%s",
                 item.getId(),
                 item.getTitle(),
                 item.getRentalType().toString(),
@@ -50,18 +68,28 @@ public class ItemUtils implements CSVUtils<Item> {
                 item.getGenre());
     }
 
-    public Item.RentalType parseRentalType(String str) {
+    /**
+     * Method to parse {@link String} of rental type into {@link Item.RentalType} enum
+     *
+     * @author Do Khoa Nguyen
+     */
+    public Item.RentalType parseRentalType(String str) throws IllegalArgumentException {
         if (str.equalsIgnoreCase("game")) return Item.RentalType.GAME;
         if (str.equalsIgnoreCase("record")) return Item.RentalType.RECORD;
         if (str.equalsIgnoreCase("dvd")) return Item.RentalType.DVD;
 
-        return null;
+        throw new IllegalArgumentException("Input rental type doesn't match any enum");
     }
 
-    public Item.LoanType parseLoanType(String str) {
+    /**
+     * Method to parse {@link String} of loan type into {@link Item.LoanType} enum
+     *
+     * @author Do Khoa Nguyen
+     */
+    public Item.LoanType parseLoanType(String str) throws IllegalArgumentException {
         if (str.equalsIgnoreCase("2-day")) return Item.LoanType.TWO_DAY;
         if (str.equalsIgnoreCase("1-week")) return Item.LoanType.ONE_WEEK;
 
-        return null;
+        throw new IllegalArgumentException("Input loan type doesn't match any enum");
     }
 }
