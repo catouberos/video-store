a68b6cb9afde7f752de6621e00dbd54ff218a012
Harry <tamkt2274@hotmail.com>
2023-05-07 19:17:48 +0700

    feat: implement initial design for login and sign up page

--- src/main/java/pls_no_shinobu/videostore/HelloApplication.java
+++ src/main/java/pls_no_shinobu/videostore/HelloApplication.java
@@ -18,8 +18,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader =
-                new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+                new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
