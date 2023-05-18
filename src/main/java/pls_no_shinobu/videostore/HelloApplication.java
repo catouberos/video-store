701138dfc5900c68cb9876f7b9bcfe711d9d1bee
sonTungN <nguyensontungtdn1901@gmail.com>
2023-05-18 18:18:45 +0700

    feat: implement design for editProfile and fix design userDashboard

--- src/main/java/pls_no_shinobu/videostore/HelloApplication.java
+++ src/main/java/pls_no_shinobu/videostore/HelloApplication.java
@@ -18,7 +18,7 @@ import java.net.URL;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userdashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         URL stylesheet = HelloApplication.class.getResource("css/main.css");
 
