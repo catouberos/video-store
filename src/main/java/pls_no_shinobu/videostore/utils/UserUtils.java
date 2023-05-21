84e4ee5950b137cb05bb7a80a4b4c18743c3abec
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-22 03:06:57 +0700

    feat: complete `UserUtils` parse

--- src/main/java/pls_no_shinobu/videostore/utils/UserUtils.java
+++ src/main/java/pls_no_shinobu/videostore/utils/UserUtils.java
@@ -7,26 +7,36 @@
 */
 package pls_no_shinobu.videostore.utils;
 
+import pls_no_shinobu.videostore.manager.ItemManager;
+import pls_no_shinobu.videostore.model.Item;
 import pls_no_shinobu.videostore.model.User;
-
-// TODO: major feature missing, as there are no way for reading Rentals from a two-line string.
 
 /**
  * Utilities for parsing, and serialize {@link User} object into CSV-compatible file
  *
  * @author Do Khoa Nguyen
  */
-public class UserUtils implements ObjectUtils<User> {
+public class UserUtils {
     /**
      * Method to parse an {@link User} from CSV-like file. <br>
-     * File format: {@code id,name,address,phone,no_of_rental,type,username,password}
+     * File format: {@code id,name,address,phone,no_of_rental,type,username,password,[rentals-ids,]}
      *
      * @author Do Khoa Nguyen
      */
-    public User parse(String str) {
+    public User parse(String str, ItemManager itemManager) {
+        // TODO: this is a kind-of dangerous way to parse a User, which should be improved given
+        // that there's time.
         try {
+            User user;
+
+            // parse the information first
             String[] tokens = str.split("\\s*,\\s*");
 
+            /*
+            there should be an easier way to do with OpenCSV CsvBindByPosition
+            https://opencsv.sourceforge.net/apidocs/com/opencsv/bean/CsvBindByPosition.html
+            although this takes a minimal approach
+             */
             String id = tokens[0];
             String name = tokens[1];
             String address = tokens[2];
@@ -36,9 +46,23 @@ public class UserUtils implements ObjectUtils<User> {
             String username = tokens[6];
             String password = tokens[7];
 
-            return new User(id, name, address, phone, rentalCount, role, username, password);
+            // initialize a user
+            user = new User(id, name, address, phone, rentalCount, role, username, password);
+
+            // then parse the rentals
+            if (tokens.length > 8) {
+                for (int i = 8; i <= tokens.length - 1; i++) {
+                    Item item = itemManager.getEntity(tokens[i]);
+
+                    if (item != null) user.addRental(item);
+                }
+            }
+
+            // return the user
+            return user;
         } catch (Exception e) {
-            // TODO: handle exception
+            // TODO: log
+
             return null;
         }
     }
@@ -50,16 +74,23 @@ public class UserUtils implements ObjectUtils<User> {
      * @author Do Khoa Nguyen
      */
     public String serialize(User user) {
-        return String.format(
-                "%s,%s,%s,%s,%d,%s,%s,%s",
-                user.getId(),
-                user.getName(),
-                user.getAddress(),
-                user.getPhone(),
-                user.getRentalCount(),
-                user.getRole().toString(),
-                user.getUsername(),
-                user.getPassword());
+        String str =
+                String.format(
+                        "%s,%s,%s,%s,%d,%s,%s,%s",
+                        user.getId() == null ? "" : user.getId(),
+                        user.getName() == null ? "" : user.getName(),
+                        user.getAddress() == null ? "" : user.getAddress(),
+                        user.getPhone() == null ? "" : user.getPhone(),
+                        user.getRentalCount(),
+                        user.getRole().toString(),
+                        user.getUsername(),
+                        user.getPassword());
+
+        for (Item item : user.getRentals()) {
+            str = str.concat("," + item.getId());
+        }
+
+        return str;
     }
 
     public static User.UserType parseUserType(String str) {
