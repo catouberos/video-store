8105706f41b256ae8216b91e77b3075cf4b7563c
Nguyen Do <khoanguyen.do@outlook.com>
2023-05-22 03:13:24 +0700

    feat: completing project utils (incl. User and Item) (#54)
    * feat: complete `UserUtils` parse
    
    * feat: remove unused ObjectUtils interface
    
    * test: update utils tests
    
    * feat: add ability to get Item using ID
    
    * feat: add user parser to FileIOUtils

--- src/main/java/pls_no_shinobu/videostore/utils/FileIOUtils.java
+++ src/main/java/pls_no_shinobu/videostore/utils/FileIOUtils.java
@@ -7,40 +7,104 @@
 */
 package pls_no_shinobu.videostore.utils;
 
+import pls_no_shinobu.videostore.manager.ItemManager;
 import pls_no_shinobu.videostore.model.Item;
 import pls_no_shinobu.videostore.model.User;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileIOUtils {
-    public ArrayList<Item> parseItem() {
+    public ArrayList<Item> parseItem() throws NullPointerException, FileNotFoundException {
         ItemUtils utils = new ItemUtils();
         ArrayList<Item> items = new ArrayList<>();
 
-        try {
-            // TODO: dynamically set file src
-            File itemsFile = new File(System.getProperty("user.dir") + "/data/items.csv");
-            Scanner scanner = new Scanner(itemsFile);
+        // TODO: dynamically set file src
+        File itemsFile = new File(System.getProperty("user.dir") + "/data/items.csv");
+        Scanner scanner = new Scanner(itemsFile);
 
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
 
-                if (!line.startsWith("#")) {
-                    Item item = utils.parse(line);
+            if (!line.startsWith("#")) {
+                Item item = utils.parse(line);
 
-                    items.add(item);
-                }
+                items.add(item);
             }
-        } catch (Exception e) {
-            // TODO: do something
         }
 
         return items;
     }
 
-    public ArrayList<User> parseUser() {
-        return null;
+    public ArrayList<User> parseUser(ItemManager itemManager)
+            throws NullPointerException, FileNotFoundException {
+        UserUtils utils = new UserUtils();
+        ArrayList<User> users = new ArrayList<>();
+
+        // TODO: dynamically set file src
+        File usersFile = new File(System.getProperty("user.dir") + "/data/users.csv");
+        Scanner scanner = new Scanner(usersFile);
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+
+            if (!str.startsWith("#")) {
+                User user = utils.parse(str, itemManager);
+
+                users.add(user);
+            }
+        }
+
+        return users;
+    }
+
+    public boolean serializeItems(ArrayList<Item> items) {
+        ItemUtils utils = new ItemUtils();
+        try {
+            // TODO: dynamically set file src
+            File itemsFile = new File(System.getProperty("user.dir") + "/data/items.csv");
+
+            PrintWriter writer = new PrintWriter(itemsFile);
+
+            for (Item item : items) {
+                writer.println(utils.serialize(item));
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Something went wrong while serialize items to file.");
+            e.printStackTrace();
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean serializeUsers(ArrayList<User> users) {
+        UserUtils utils = new UserUtils();
+
+        try {
+            // TODO: dynamically set file src
+            File itemsFile = new File(System.getProperty("user.dir") + "/data/users.csv");
+
+            PrintWriter writer = new PrintWriter(itemsFile);
+
+            for (User user : users) {
+                writer.println(utils.serialize(user));
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Something went wrong while serialize users to file.");
+            e.printStackTrace();
+
+            return false;
+        }
+
+        return true;
     }
 }
