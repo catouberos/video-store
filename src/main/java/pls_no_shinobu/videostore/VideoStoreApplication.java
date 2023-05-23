dacdda2141cdba294741e2a32eaf4e908d2df9cd
Nguyen Do <khoanguyen.do@outlook.com>
2023-05-24 00:09:20 +0700

    feat: display donus as icon (#69)
    * feat: add donut icon
    
    * build: remove unused assets
    
    * feat: display donut icon for application
    
    * build: spotless apply

--- src/main/java/pls_no_shinobu/videostore/VideoStoreApplication.java
+++ src/main/java/pls_no_shinobu/videostore/VideoStoreApplication.java
@@ -10,19 +10,22 @@ package pls_no_shinobu.videostore;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import pls_no_shinobu.videostore.core.CSVDatabase;
 import pls_no_shinobu.videostore.core.Session;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 public class VideoStoreApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        CSVDatabase database = CSVDatabase.getInstance();
-        Session session = Session.getInstance();
+        // create a database and session instance at first
+        CSVDatabase.getInstance();
+        Session.getInstance();
 
         FXMLLoader fxmlLoader =
                 new FXMLLoader(VideoStoreApplication.class.getResource("signin.fxml"));
@@ -35,6 +38,16 @@ public class VideoStoreApplication extends Application {
         stage.setMinWidth(600);
         stage.setMinHeight(400);
         stage.setScene(scene);
+
+        try {
+            InputStream iconStream =
+                    VideoStoreApplication.class.getResourceAsStream("img/donut.png");
+            if (iconStream != null) stage.getIcons().add(new Image(iconStream));
+        } catch (NullPointerException e) {
+            System.err.println("Cannot set icon, details: " + e.getMessage());
+            e.printStackTrace();
+        }
+
         stage.show();
     }
 
