055005082850814d8146effc62251626599a3d49
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-24 21:07:59 +0700

    feat: added help scene

--- src/main/java/pls_no_shinobu/videostore/controller/UserDashboardController.java
+++ src/main/java/pls_no_shinobu/videostore/controller/UserDashboardController.java
@@ -71,6 +71,7 @@ public class UserDashboardController {
     @FXML private VBox profileContainer;
     @FXML private VBox rentedContainer;
     @FXML private HBox randomContainer;
+    @FXML private ScrollPane helpContainer;
 
     @FXML private TableView<Item> itemTable;
     @FXML private TableView<Item> rentedTable;
@@ -341,6 +342,12 @@ public class UserDashboardController {
         initializeItemTable();
         initializeRentedTable();
         setRandomItem();
+    }
+
+    @FXML
+    public void onHelpButtonClick() {
+        titleText.setText("Info and FAQs");
+        PaneUtils.setPane(stackPane, helpContainer);
     }
 
     @FXML
