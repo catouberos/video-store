eb7fc24372a3747075ff4774f2bc4afbe5bfec77
MKHOAA\Custon <nguyenminhkhoa77284@gmail.com>
2023-05-26 14:44:12 +0700

    feat: add a transition to the `setPane` method

--- src/main/java/pls_no_shinobu/videostore/controller/utils/PaneUtils.java
+++ src/main/java/pls_no_shinobu/videostore/controller/utils/PaneUtils.java
@@ -7,9 +7,11 @@
 */
 package pls_no_shinobu.videostore.controller.utils;
 
+import javafx.animation.FadeTransition;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class PaneUtils {
     /**
@@ -18,10 +20,24 @@ public class PaneUtils {
      * @author Do Khoa Nguyen
      */
     public static void setPane(StackPane stackPane, Node content) {
-        // hide every node in stackPane
-        for (Node node : stackPane.getChildren()) node.setVisible(false);
+        // hide every node in stackPane with fade out transition
+        for (Node node : stackPane.getChildren()) {
+            if (node == content) continue;
 
-        // then, shows only the specified one
+            if (node.isVisible()) {
+                FadeTransition fadeOut = new FadeTransition(Duration.millis(500), node);
+                fadeOut.setFromValue(1.0);
+                fadeOut.setToValue(0.0);
+                fadeOut.setOnFinished(event -> node.setVisible(false));
+                fadeOut.play();
+            }
+        }
+
+        // then, shows only the specified one with fade in transition
         content.setVisible(true);
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(500), content);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
     }
 }
